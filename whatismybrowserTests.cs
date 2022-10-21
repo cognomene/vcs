2e2bb9ebd8aba079dee8752a4add38405e7b28bb
EglePe <116336728+cognomene@users.noreply.github.com>
2022-10-21 12:50:48 +0300

    Create whatismybrowserTests.cs
    15 Namų darbai

---
+++ whatismybrowserTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace NamuDarbai
+{
+
+    class WhatIsMyBrowser
+    {
+
+        [Test]
+        public static void FirefoxTest()
+        {
+            IWebDriver firefoxDriver = new FirefoxDriver();
+            firefoxDriver.Url = "https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent";
+            firefoxDriver.Manage().Window.Maximize();
+
+
+            IWebElement browserAndOsDetection = firefoxDriver.FindElement(By.CssSelector(".simple-major"));
+            Assert.AreEqual("Firefox 105 on macOS (Catalina)", browserAndOsDetection.Text, "Text is not the same");
+
+            firefoxDriver.Quit();
+        }
+
+
+
+        [Test]
+        public static void ChromeTest()
+        {
+            IWebDriver chromeDriver = new ChromeDriver();
+            chromeDriver.Url = "https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent";
+            chromeDriver.Manage().Window.Maximize();
+
+
+            IWebElement browserAndOsDetection = chromeDriver.FindElement(By.CssSelector(".simple-major"));
+            Assert.AreEqual("Chrome 106 on macOS (Catalina)", browserAndOsDetection.Text, "Text is not the same");
+
+            chromeDriver.Quit();
+        }
+
+
+
+    }
+}
+
